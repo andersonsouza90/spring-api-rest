a2d266ee280f62ff1071a92ce74435e0036f0de7
andersonsouza90 <andersonsilva.souza90@gmail.com>
2021-06-05 01:07:36 -0300

    configuração inicial do projeto

---
+++ src/main/java/com/api/rest/SpringrestapiApplication.java
@@ -0,0 +1,25 @@
+package com.api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EntityScan(basePackages = {"com.api.rest.model"})
+@ComponentScan(basePackages = {"com.*"})
+@EnableJpaRepositories(basePackages = {"com.api.rest.repository"})
+@EnableTransactionManagement
+@EnableWebMvc
+@RestController
+public class SpringrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestapiApplication.class, args);
+	}
+
+}
